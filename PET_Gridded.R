91be982859cac68b5001590f9cc1c3b106706f37
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-11-17 23:29:17 -0300

    Support for terra package

--- PET_Gridded.R
+++ PET_Gridded.R
@@ -5,31 +5,38 @@ library("raster")
 library("readxl")
 library("Evapotranspiration")
 
-t2m_max_c2<-stack("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Data/TMAX_Corrected.nc")
-t2m_min_c2<-stack("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Data/TMIN_Corrected.nc")
+period <- seq(from = as.POSIXct('2006-04-01', tz="UTC"), to = as.POSIXct('2019-03-31', tz="UTC"), by = "day")
+t2m_max_c2<-rast("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Climate/TMAX_Corrected.nc")
+t2m_min_c2<-rast("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Climate/TMIN_Corrected.nc")
+dem_hr<-rast("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/DEMs/dem_AL_125f3.tif")
 
 data("constants")
 constants$lat_rad <- -37*3.14/180
-constants$Elev <- mean(getValues(dem_hr))
+constants$Elev <- mean(values(dem_hr))
 
-PET_HS<-brick(t2m_max_c2, values=FALSE)
-for (i in 1:ncell(t2m_max_c2)) {
+PET_HS<-rast(t2m_max_c2, props=TRUE)
+for (i in 1:nrow(t2m_max_c2)) {
+  for (j in 1:ncol(t2m_max_c2)) {
   
-  climatedata<-data.frame("Tmax" = as.numeric(t2m_max_c2[i]), 
-                          "Tmin" = as.numeric(t2m_min_c2[i]), 
+  climatedata<-data.frame("Tmax" = as.numeric(t2m_max_c2[i,j]), 
+                          "Tmin" = as.numeric(t2m_min_c2[i,j]), 
                           "Month" = as.numeric(format(period, "%m")),
                           "Day" = as.numeric(format(period, "%d")),
                           "Year" = as.numeric(format(period, "%Y")))
   
   climatedata<-ReadInputs(varnames= c("Tmax", "Tmin"), climatedata, constants, stopmissing =c(10,10,3), timestep = "daily", message = "no")
-  PET_HS[i] <- as.numeric(ET.HargreavesSamani(climatedata, constants, ts="daily", message="no", AdditionalStats="no", save.csv="no")$ET.Daily)
+  PET_HS[i,j] <- t(as.data.frame(ET.HargreavesSamani(climatedata, constants, ts="daily", message="no", AdditionalStats="no", save.csv="no")$ET.Daily))
   print(i)
+  }
 }
 
-PET_HS<-setZ(PET_HS, period)
+time(PET_HS)<-period
 PET_HS<-setNames(PET_HS, period)
 
-setwd("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Data/")
+PET_HS_m <- tapp(PET_HS, strftime(time(PET_HS),format="%Y"), fun = sum, na.rm = TRUE)
+PET_HS_m <- mean(PET_HS_m[[-c(1,nlyr(PET_HS_m))]])
 
-writeRaster(PET_HS, "PET_Corrected.nc", format = "CDF", overwrite=TRUE, varname="pet", varunit="mm", 
-            longname="pet", xname="X", yname="Y", zname="time", zunit="day")
+setwd("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Climate/")
+
+writeRaster(PET_HS_m, "PET_mean.tif", overwrite = TRUE)
+writeCDF(PET_HS, "PET_Corrected.nc", overwrite=TRUE, varname="pet", unit="mm", longname="Potential Evapotranspiration", zname="time", compression = 9)
--- T2M_Gridded.R
+++ T2M_Gridded.R
@@ -2,63 +2,60 @@ rm(list=ls())
 cat("\014")  
 
 library("hydroGOF")
-library("raster")
+library("terra")
 library("readxl")
 
-sample <- raster(matrix(rnorm(400),20,20))
-extent(sample) <- c(-72.79,-72.69,-37.55, -37.45)
-projection(sample) <- CRS("+init=epsg:4326")
+sample <- rast(matrix(rnorm(400),20,20), crs = "+init=epsg:4326")
+ext(sample) <- c(-72.79,-72.69,-37.55, -37.45)
 
 #Observations
-period <- seq(as.Date('2006-04-01'), as.Date('2019-03-31'), by = 'day')
-t2m_shape<-shapefile("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/PP_stations.shp")[4,]
+period <- seq(from = as.POSIXct('2006-04-01', tz="UTC"), to = as.POSIXct('2019-03-31', tz="UTC"), by = "day")
+t2m_shape<-vect("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/PP_stations.shp")[4,]
 
-t2m_data<-data.frame(read_xlsx("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Data/T2M_data.xlsx", sheet = "data", guess_max = 30000))
-t2m_data$Date<-as.Date(t2m_data$Date)
+t2m_data<-data.frame(read_xlsx("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Climate/T2M_data.xlsx", sheet = "data", guess_max = 1000))
+t2m_data$Date<-as.POSIXct(t2m_data$Date)
 t2m_data<-subset(t2m_data, Date >= min(period) & Date <= max(period))
 
 #Gridded products
-t2m_max_cr2met<-stack("E:/Datasets/CR2MET/CR2MET_tmax_v2.0_day_1979_2020_005deg.nc", varname="tmax")
-t2m_min_cr2met<-stack("E:/Datasets/CR2MET/CR2MET_tmin_v2.0_day_1979_2020_005deg.nc", varname="tmin")
-t2m_max_cr2met<-crop(t2m_max_cr2met, sample, snap='out')
-t2m_min_cr2met<-crop(t2m_min_cr2met, sample, snap='out')
+t2m_max_cr2met<-rast("E:/Datasets/CR2MET/CR2MET_tmax_v2.0_day_1979_2020_005deg.nc")
+t2m_min_cr2met<-rast("E:/Datasets/CR2MET/CR2MET_tmin_v2.0_day_1979_2020_005deg.nc")
+t2m_max_cr2met<-resample(t2m_max_cr2met, sample, method='bilinear')
+t2m_min_cr2met<-resample(t2m_min_cr2met, sample, method='bilinear')
 
 #Downscaling
-dem_hr<-projectRaster(raster("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/DEMs/dem_AL_125f3.tif"), to = sample, method="bilinear")
-dem_lr<-projectRaster(raster("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/DEMs/dem_AL_125f3.tif"), to = t2m_max_cr2met, method="bilinear")
+dem_hr<-project(rast("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/DEMs/dem_AL_125f3.tif"), sample, method="bilinear")
+dem_lr<-project(rast("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/GIS/DEMs/dem_AL_125f3.tif"), rast("E:/Datasets/CR2MET/CR2MET_tmax_v2.0_day_1979_2020_005deg.nc"), method="bilinear")
 dem_lr<-resample(dem_lr, dem_hr, method="bilinear")
 
-t2m_max_cr2met<-resample(t2m_max_cr2met, sample, method="ngb")
-t2m_min_cr2met<-resample(t2m_min_cr2met, sample, method="ngb")
 t2m_max_cr2met<-t2m_max_cr2met+(dem_lr-dem_hr)*0.0065
 t2m_min_cr2met<-t2m_min_cr2met+(dem_lr-dem_hr)*0.0065
 t2m_max_cr2met<-round(t2m_max_cr2met, 2)
 t2m_min_cr2met<-round(t2m_min_cr2met, 2)
 
-t2m_max_cr2met<-setZ(t2m_max_cr2met, seq(as.Date('1979-01-01'), as.Date('2020-04-30'), by = 'day'))
-t2m_min_cr2met<-setZ(t2m_min_cr2met, seq(as.Date('1979-01-01'), as.Date('2020-04-30'), by = 'day'))
 t2m_max_cr2met<-setNames(t2m_max_cr2met, seq(as.Date('1979-01-01'), as.Date('2020-04-30'), by = 'day'))
 t2m_min_cr2met<-setNames(t2m_min_cr2met, seq(as.Date('1979-01-01'), as.Date('2020-04-30'), by = 'day'))
-t2m_max_cr2met<-subset(t2m_max_cr2met, which(getZ(t2m_max_cr2met) >= min(period) & getZ(t2m_max_cr2met) <= max(period)))
-t2m_min_cr2met<-subset(t2m_min_cr2met, which(getZ(t2m_min_cr2met) >= min(period) & getZ(t2m_min_cr2met) <= max(period)))
+t2m_max_cr2met<-subset(t2m_max_cr2met, which(time(t2m_max_cr2met) >= min(period) & time(t2m_max_cr2met) <= max(period)))
+t2m_min_cr2met<-subset(t2m_min_cr2met, which(time(t2m_min_cr2met) >= min(period) & time(t2m_min_cr2met) <= max(period)))
 
 #Extract, measure performance 
-t2m_max_cr2met_i<-t(raster::extract(t2m_max_cr2met,t2m_shape, method='simple'))
-t2m_min_cr2met_i<-t(raster::extract(t2m_min_cr2met,t2m_shape, method='simple'))
+t2m_max_cr2met_i<-as.numeric(t(terra::extract(t2m_max_cr2met,t2m_shape)))
+t2m_min_cr2met_i<-as.numeric(t(terra::extract(t2m_min_cr2met,t2m_shape)))
+t2m_max_cr2met_i<-t2m_max_cr2met_i[2:length(t2m_max_cr2met_i)]
+t2m_min_cr2met_i<-t2m_min_cr2met_i[2:length(t2m_min_cr2met_i)]
 
-KGE_cr2met_tmax <-KGE(sim=as.numeric(t2m_max_cr2met_i), obs=t2m_data$T2M_max, method="2012", out.type="full",na.rm=TRUE)
-KGE_cr2met_tmax <- as.data.frame(cbind(t(KGE_cr2met_tmax$KGE.elements),KGE_cr2met_tmax$KGE.value))
-KGE_cr2met_tmin <-KGE(sim=as.numeric(t2m_min_cr2met_i), obs=t2m_data$T2M_min, method="2012", out.type="full",na.rm=TRUE)
-KGE_cr2met_tmin <- as.data.frame(cbind(t(KGE_cr2met_tmin$KGE.elements),KGE_cr2met_tmin$KGE.value))
-KGE_cr2met_tmax$ME<-me(sim=as.numeric(t2m_max_cr2met_i), obs=t2m_data$T2M_max, na.rm=TRUE)
-KGE_cr2met_tmin$ME<-me(sim=as.numeric(t2m_min_cr2met_i), obs=t2m_data$T2M_min, na.rm=TRUE)
+KGE_cr2met_tmax     <-KGE(sim=t2m_max_cr2met_i, obs=t2m_data$T2M_max, method="2012", out.type="full",na.rm=TRUE)
+KGE_cr2met_tmax     <- as.data.frame(cbind(t(KGE_cr2met_tmax$KGE.elements),KGE_cr2met_tmax$KGE.value))
+KGE_cr2met_tmin     <-KGE(sim=t2m_min_cr2met_i, obs=t2m_data$T2M_min, method="2012", out.type="full",na.rm=TRUE)
+KGE_cr2met_tmin     <- as.data.frame(cbind(t(KGE_cr2met_tmin$KGE.elements),KGE_cr2met_tmin$KGE.value))
+KGE_cr2met_tmax$ME  <-me(sim=t2m_max_cr2met_i, obs=t2m_data$T2M_max, na.rm=TRUE)
+KGE_cr2met_tmin$ME  <-me(sim=t2m_min_cr2met_i, obs=t2m_data$T2M_min, na.rm=TRUE)
 
 #Downscaling: Mean and variance scaling 
 me_tmax<-KGE_cr2met_tmax$ME
 me_tmin<-KGE_cr2met_tmin$ME
 
-rSD_tmax<-1/rSD(sim=as.numeric(t2m_max_cr2met_i)-me_tmax, obs=t2m_data$T2M_max, na.rm=TRUE)
-rSD_tmin<-1/rSD(sim=as.numeric(t2m_min_cr2met_i)-me_tmin, obs=t2m_data$T2M_min, na.rm=TRUE)
+rSD_tmax<-1/rSD(sim=t2m_max_cr2met_i-me_tmax, obs=t2m_data$T2M_max, na.rm=TRUE)
+rSD_tmin<-1/rSD(sim=t2m_min_cr2met_i-me_tmin, obs=t2m_data$T2M_min, na.rm=TRUE)
 
 t2m_max_c1<-t2m_max_cr2met - me_tmax
 t2m_max_c2<-t2m_max_c1-mean(t2m_max_c1)
@@ -69,32 +66,30 @@ t2m_min_c1<-t2m_min_cr2met - me_tmin
 t2m_min_c2<-t2m_min_c1-mean(t2m_min_c1)
 t2m_min_c2<-t2m_min_c2*rSD_tmin
 t2m_min_c2<-t2m_min_c2 + mean(t2m_min_c1)
-
-t2m_max_c2<-setZ(t2m_max_c2, period)
-t2m_min_c2<-setZ(t2m_min_c2, period)
 t2m_max_c2<-setNames(t2m_max_c2, period)
 t2m_min_c2<-setNames(t2m_min_c2, period)
-t2m_max_c2<-stack(t2m_max_c2)
-t2m_min_c2<-stack(t2m_min_c2)
+
+t2m_max_c2_m <- tapp(t2m_max_c2, strftime(time(t2m_max_c2),format="%Y"), fun = mean)
+t2m_max_c2_m <- mean(t2m_max_c2_m[[-c(1,nlyr(t2m_max_c2_m))]])
+t2m_min_c2_m <- tapp(t2m_min_c2, strftime(time(t2m_min_c2),format="%Y"), fun = mean)
+t2m_min_c2_m <- mean(t2m_min_c2_m[[-c(1,nlyr(t2m_min_c2_m))]])
+t2m_mean<-mean(c(t2m_max_c2_m, t2m_min_c2_m))
 
 #New validation: It works!
-t2m_max_cr2met_i<-t(raster::extract(t2m_max_c2,t2m_shape, method='simple'))
-t2m_min_cr2met_i<-t(raster::extract(t2m_min_c2,t2m_shape, method='simple'))
+t2m_max_cr2met_i<-as.numeric(t(terra::extract(t2m_max_c2,t2m_shape)))
+t2m_min_cr2met_i<-as.numeric(t(terra::extract(t2m_min_c2,t2m_shape)))
+t2m_max_cr2met_i<-t2m_max_cr2met_i[2:length(t2m_max_cr2met_i)]
+t2m_min_cr2met_i<-t2m_min_cr2met_i[2:length(t2m_min_cr2met_i)]
 
-KGE_cr2met_tmax <-KGE(sim=as.numeric(t2m_max_cr2met_i), obs=t2m_data$T2M_max, method="2012", out.type="full",na.rm=TRUE)
-KGE_cr2met_tmax <- as.data.frame(cbind(t(KGE_cr2met_tmax$KGE.elements),KGE_cr2met_tmax$KGE.value))
-KGE_cr2met_tmin <-KGE(sim=as.numeric(t2m_min_cr2met_i), obs=t2m_data$T2M_min, method="2012", out.type="full",na.rm=TRUE)
-KGE_cr2met_tmin <- as.data.frame(cbind(t(KGE_cr2met_tmin$KGE.elements),KGE_cr2met_tmin$KGE.value))
-KGE_cr2met_tmax$ME<-me(sim=as.numeric(t2m_max_cr2met_i), obs=t2m_data$T2M_max, na.rm=TRUE)
-KGE_cr2met_tmin$ME<-me(sim=as.numeric(t2m_min_cr2met_i), obs=t2m_data$T2M_min, na.rm=TRUE)
+KGE_cr2met_tmax    <-KGE(sim=t2m_max_cr2met_i, obs=t2m_data$T2M_max, method="2012", out.type="full",na.rm=TRUE)
+KGE_cr2met_tmax    <- as.data.frame(cbind(t(KGE_cr2met_tmax$KGE.elements),KGE_cr2met_tmax$KGE.value))
+KGE_cr2met_tmin    <-KGE(sim=t2m_min_cr2met_i, obs=t2m_data$T2M_min, method="2012", out.type="full",na.rm=TRUE)
+KGE_cr2met_tmin    <- as.data.frame(cbind(t(KGE_cr2met_tmin$KGE.elements),KGE_cr2met_tmin$KGE.value))
+KGE_cr2met_tmax$ME <-me(sim=t2m_max_cr2met_i, obs=t2m_data$T2M_max, na.rm=TRUE)
+KGE_cr2met_tmin$ME <-me(sim=t2m_min_cr2met_i, obs=t2m_data$T2M_min, na.rm=TRUE)
 
-setwd("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Data/")
+setwd("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Climate/")
 
-writeRaster(t2m_max_c2, "TMAX_Corrected.nc", format = "CDF", overwrite=TRUE, varname="tmax", varunit="degC", 
-            longname="tmac", xname="X", yname="Y", zname="time", zunit="day")
-
-writeRaster(t2m_min_c2, "TMIN_Corrected.nc", format = "CDF", overwrite=TRUE, varname="tmin", varunit="degC", 
-            longname="tmin", xname="X", yname="Y", zname="time", zunit="day")
-
-
-
+writeRaster(t2m_mean, "T2M_mean.tif", overwrite = TRUE)
+writeCDF(t2m_max_c2, "TMAX_Corrected.nc", overwrite=TRUE, varname="t2m_max", unit="degC", longname="Maximun temperature", zname="time", compression = 9)
+writeCDF(t2m_min_c2, "TMIN_Corrected.nc", overwrite=TRUE, varname="t2m_min", unit="degC", longname="Minimum temperature", zname="time", compression = 9)

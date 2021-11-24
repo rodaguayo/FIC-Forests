3806dbf298540ea48f1488a754dc608d8aefe2f5
Rodrigo Aguayo <roodaguayo@gmail.com>
2021-11-23 21:47:20 -0300

    add slope as + o -

--- Figures/Figure_Metrics_heatmap.R
+++ Figures/Figure_Metrics_heatmap.R
@@ -10,6 +10,7 @@ data<-read.csv("C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Results/Change_p
 data$Metric<-as.factor(data$Metric)
 data$ID<-as.factor(data$ID)
 data$P_value<-as.factor(data$P_value)
+data$Trend[is.na(data$Trend)]<-" "
 
 f <- list(family = "Verdana", size = 22)
 f2 <- list(family = "Verdana", size = 16) 
@@ -18,9 +19,11 @@ x <- list(titlefont = f, tickfont = f2, ticks = "outside")
 y <- list(titlefont = f, tickfont = f2, ticks = "outside")
 tiple <- list(text = "P-value", titlefont = f, tickfont = f2, ticks = "outside")
 
-fig <- plot_ly(data, x = ~Metric, y = ~ID, z = ~P_value, type = "heatmap", colorscale  = "Viridis", colorbar = list(title = tiple))
+fig <- plot_ly(data, y = ~Metric, x = ~ID, z = ~P_value, type = "heatmap", colorscale  = "Viridis", colorbar = list(title = tiple))
 fig <- fig %>% layout(xaxis = x, yaxis = y)
+fig <- fig %>% add_annotations(x = data$ID, y = data$Metric, text =  data$Trend, showarrow = FALSE, font=list(color='white', size = 19))
 fig
+
 
 server <- orca_serve()
 server$export(fig, file = "C:/Users/rooda/Dropbox/Proyectos/Nacimiento FIC/Figures/Figure7_PET_PP.png", width = 700, height = 1000, scale = 3)
